a81245a28d20f9591e147b028c6c2ecee7d10b89
Daguhh <david@debian-david.home>
2020-01-25 11:22:18 +0100

    ajout logo et bordure

--- read_exif_test.py
+++ read_exif_test.py
@@ -6,85 +6,86 @@ import datetime
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ExifTags import TAGS
 
-# exif decimal code
-DateTimeOriginal = 36867 # DateTimeOriginal
-
-# paramètres ################################
-path = "/home/david/Programmation/SmallScripts/"
+############## Parameters ###############
+path = "/home/david/Programmation/SmallScripts/text_on_photo/"
 font_type = 'Comic_Sans_MS.ttf' # arial.ttf Comic_Sans_MS.ttf    Times_New_Roman.ttf  Verdana.ttf
 
-# position des éléments
-def none(im) :
-    return "test"
+file_name = "DSC00968.JPG"
+logo_name = "debian_logo.jpeg"
+logo_name_2 = "openlogo-nd-100.png"
 
-text1 = {"value" : "BlaBla!!!",
-         "fontsize" : 5/100,
-         "y_pos" : 5/100,
-         "x_pos" : 50/100,
-         "metadata" : none}
+text_yr = 5/100 # relative y position
+text_xr = 50/100 # relative x position
+text_size_r = 5/100 # relative image/text fontsize
 
-def get_date(im) :
+logo_xr = 0/100
+logo_yr = 100/100
+logo_size_r = 15/100
+
+border_size_r = 2/100
+border_color = (255,255,255) # white
+
+DateTimeOriginal = 36867 # DateTimeOriginal exif decimal code
+
+########### functions ################
+def get_date_from_metadata(im) :
     exif_datas = im.getexif()
     #print(TAGS[DateTimeOriginal])
     date = exif_datas[DateTimeOriginal]
     date = datetime.datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-    date = [date.strftime('%d/%m/%Y'), date.strftime('%HH%M')]
-    return date
+    day = date.strftime('%d/%m/%Y')
+    hour = date.strftime('%HH%M')
+    return day, hour
 
-text2 = {"value" : "BlaBla!!!, réalisée le {} à {}",
-         "fontsize" : 5/100,
-         "y_pos" : 95/100,
-         "x_pos" : 5/100,
-         "metadata" : get_date}
-
-
-texts = [text1, text2]
-
-# program ###################################
+########### Start #################
 #file_list = os.listdir(path)
 #os.makedirs(path + "processed")
 
-file_name = "DSC00968.JPG"
-if 1 :
-#for file_name in file_list :
-    #### Image ####
-    # on ouvre l'image
+if 1 : #for file_name in file_list :
+
     im = Image.open(path + file_name)
     draw = ImageDraw.Draw(im)
 
-    # dimension de l'image
     im_w, im_h = im.size
 
+    ############## Text ##############
+    day, hour = get_date_from_metadata(im)
+    text_displayed = "BlaBla!!!, réalisée le {} à {}".format(day,hour)
+    print(text_displayed)
 
-    for text in texts :
+    font_size = round(im_h * text_size_r)
+    font = ImageFont.truetype(font_type, font_size)
 
-        # obtention des metadatas
-        date = text['metadata'](im)
+    text_w, text_h = font.getsize(text_displayed)
 
+    x = im_w*(text_xr) - text_w/2
+    y = im_h*(1-text_yr) - text_h/2
 
-        #### Texte ####
-        # texte à afficher
-        text_displayed = text['value'].format(*date)
+    draw.rectangle([(x, y), (x + text_w, y + text_h)], fill=(0,0,0,128))
+    draw.text((x, y), text_displayed, font=font)
 
-        # calcul taille de police
-        font_size = round(im_h * text['fontsize'])
-        font = ImageFont.truetype(font_type, font_size)
+    ############## Logo ##############
+    logo_im = Image.open(path + logo_name_2)
 
-        # determination des dimensions du texte
-        text_w, text_h = font.getsize(text_displayed)
-        print(text_displayed)
+    logo_w, logo_h = logo_im.size
 
-        # et de sa position
-        x = im_w*(text['x_pos']) - text_w/2 # in the middle (horizontally)
-        y = im_h*(1-text['y_pos']) - text_h/2 # à 1/text1_relative_position_y % de la hauteur de l'image
+    hw_ratio = logo_h/logo_w
+    new_w = round(im_w*logo_size_r)
+    new_h = round(hw_ratio*new_w)
 
-        # on trace le texte sur (et) un fond noir
-        draw.rectangle([(x, y), (x + text_w, y + text_h)], fill=(0,0,0,128))
-        draw.text((x, y), text_displayed, font=font)
+    logo_im = logo_im.resize((new_w, new_h))
+    x = round(im_w*logo_xr)
+    y = round(im_h*(1-logo_yr))
 
-    # sauvegarde l'image
+    im.paste(logo_im,(x,y))
 
-    im.save(path + "processed/" + file_name)
+    ########## Border ############
+    size = round(border_size_r * im_h)
+    border = Image.new("RGB", (im_w+2*size, im_h+2*size), border_color)
+    border.paste(im,(size,size))
+
+    ############### Save  ###############
+    border.save(path + "processed/" + file_name)
 
 
 
