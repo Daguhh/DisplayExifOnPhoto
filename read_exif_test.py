92f29715eb20b611027fd103e206bd7e2cbaf1c0
Daguhh <david@debian-david.home>
2020-01-25 11:42:31 +0100

    Better variable name + separate path for logo picture

--- read_exif_test.py
+++ read_exif_test.py
@@ -10,19 +10,18 @@ from PIL.ExifTags import TAGS
 path = "/home/david/Programmation/SmallScripts/text_on_photo/"
 font_type = 'Comic_Sans_MS.ttf' # arial.ttf Comic_Sans_MS.ttf    Times_New_Roman.ttf  Verdana.ttf
 
-file_name = "DSC00968.JPG"
-logo_name = "debian_logo.jpeg"
-logo_name_2 = "openlogo-nd-100.png"
+logo_name = "openlogo-nd-100.png"
+logo_path = "/home/david/Programmation/SmallScripts/text_on_photo/"
 
-text_yr = 5/100 # relative y position
-text_xr = 50/100 # relative x position
-text_size_r = 5/100 # relative image/text fontsize
+text_yr = 5/100 # text relative y position
+text_xr = 50/100 # tex relative x position
+text_size_r = 5/100 # text relative image/text fontsize
 
-logo_xr = 0/100
+logo_xr = 0/100 # logo relative position
 logo_yr = 100/100
-logo_size_r = 15/100
+logo_size_r = 15/100 # logo relative size (to image height)
 
-border_size_r = 2/100
+border_size_r = 2/100 # border relative size (to image height)
 border_color = (255,255,255) # white
 
 DateTimeOriginal = 36867 # DateTimeOriginal exif decimal code
@@ -38,6 +37,7 @@ def get_date_from_metadata(im) :
     return day, hour
 
 ########### Start #################
+file_name = "DSC00968.JPG"
 #file_list = os.listdir(path)
 #os.makedirs(path + "processed")
 
@@ -51,21 +51,21 @@ if 1 : #for file_name in file_list :
     ############## Text ##############
     day, hour = get_date_from_metadata(im)
     text_displayed = "BlaBla!!!, réalisée le {} à {}".format(day,hour)
-    print(text_displayed)
+    #print(text_displayed)
 
     font_size = round(im_h * text_size_r)
     font = ImageFont.truetype(font_type, font_size)
 
     text_w, text_h = font.getsize(text_displayed)
 
-    x = im_w*(text_xr) - text_w/2
+    x = im_w*(text_xr) - text_w/2 # position from text area center
     y = im_h*(1-text_yr) - text_h/2
 
     draw.rectangle([(x, y), (x + text_w, y + text_h)], fill=(0,0,0,128))
     draw.text((x, y), text_displayed, font=font)
 
     ############## Logo ##############
-    logo_im = Image.open(path + logo_name_2)
+    logo_im = Image.open(logo_path + logo_name)
 
     logo_w, logo_h = logo_im.size
 
@@ -74,7 +74,7 @@ if 1 : #for file_name in file_list :
     new_h = round(hw_ratio*new_w)
 
     logo_im = logo_im.resize((new_w, new_h))
-    x = round(im_w*logo_xr)
+    x = round(im_w*logo_xr) # position from logo upper left corner
     y = round(im_h*(1-logo_yr))
 
     im.paste(logo_im,(x,y))
